b1bc1bc5953523af45f9ffa7f5e14a4d419b0e50
brennora <brennora@gmail.com>
2019-12-07 18:57:27 -0500

    adding profiles and zodiac info

--- interface/profile/data.js
+++ interface/profile/data.js
@@ -1,22 +1,33 @@
 /**
  * Course: COMP 426
- * Assignment: a05
  *
- * This file defines data to be rendered to the page.
+ * This file defines data of profile pages.
  */
-
-
 /**
- * Represents a list of heroic users
+ * Represents a list of user profiles (based on instagram friends)
  */
 const profileData = [{
-    id: 1,
-    age: "26",
-    name: "Marcus",
+    id: marcus,
+    age: "22",
+    dob: 10-07-1997,
+    zodiac: libra,
+    matches: [gemini, sagittarius, aries, aquarius, leo, libra],
+    name: "Marcus Vela",
     img: "../../images/marcus.jpeg",
-    color: "purple",
-    backgroundColor: "#000000",
-    subtitle: "Aries",
-    description: "My ideal date? I pick you up in my car, and there’s candles gently licking the air on the dashboard ‘Rich, there’s candles on the dashboard’, I smile. ‘Yeah, I know.’ We take a drive, go to a restaurant, have a wonderful meal and talk about life, goals, and ideals.  As we leave, you notice my car is ablaze. ‘Rich, your car is on fire!’. ‘It’s okay – it’s not mine’ – at which point I pull out marshmallows. We cook them & eat them. Then I kiss you passionately. In front of the burning car.",
+    bio: "My ideal date? I pick you up in my car, and there’s candles gently licking the air on the dashboard ‘Rich, there’s candles on the dashboard’, I smile. ‘Yeah, I know.’ We take a drive, go to a restaurant, have a wonderful meal and talk about life, goals, and ideals.  As we leave, you notice my car is ablaze. ‘Rich, your car is on fire!’. ‘It’s okay – it’s not mine’ – at which point I pull out marshmallows. We cook them & eat them. Then I kiss you passionately. In front of the burning car.",
     interests: "skiing, cooking, Netflix & Chill",
-},];
+    insta: www.instagram.com,
+},
+{
+    id: brennora,
+    age: "20",
+    dob: 11-03-1999,
+    zodiac: scorpio,
+    matches: [gemini, sagittarius, aries, aquarius, leo, libra],
+    name: "Brennora Cameron",
+    img: "../../images/galaxy.jpeg",
+    bio: "I just wanna smoke, eat chicken wings, and listen to R&B",
+    interests: "dancing, coding, zumba",
+    insta: www.instagram.com,
+},
+];

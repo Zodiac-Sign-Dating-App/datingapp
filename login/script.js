cfa9eefc504c2d560f84daaa3039e5f114183a95
Jairo-Rubio <jairorub@live.unc.edu>
2019-12-10 22:10:11 -0500

    fixed login

--- login/script.js
+++ login/script.js
@@ -1,6 +1,57 @@
+
+
 $(function() {
   const $form = $('#login-form');
   const $message = $('#message');
+
+const second = function(){
+  let token = sessionStorage.getItem('jwt');
+      $.ajax({
+        url: 'http://localhost:3000/account/status',
+        method: 'get',
+      
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        withCredentials: true,
+        success: function(data){
+          res = data;
+        }
+      }).then(() => {
+
+        // console.log(res.user);
+        sessionStorage.setItem('age', res.user.age);
+        sessionStorage.setItem('sign', res.user.sign);
+        third();
+      }).catch(() => {
+        $message.html('<span class="has-text-danger">Something went wrong and you were not verified. Check your email and password and your internet connection.</span>');
+      });
+      
+};  
+
+const third = async function(){
+  let token = sessionStorage.getItem('jwt');
+  let profile= {};
+  try{    
+  const result = await axios({
+          url: 'http://localhost:3000/user/profile',
+          method: 'get',
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+  });
+  
+  profile = result.data.result;
+  sessionStorage.setItem('name', profile.name);
+  sessionStorage.setItem('interests', profile.interests);
+  sessionStorage.setItem('bio', profile.bio);
+  location.href = ("../interface/profile/index.html");
+  } catch(error){
+    console.log(error);
+    location.href = ("../interface/profile/index.html");
+  }
+
+};
 
   $form.submit(function(e) {
     e.preventDefault();
@@ -31,50 +82,9 @@ $(function() {
       const name = res.name;
       sessionStorage.setItem('jwt', jwt);
       sessionStorage.setItem('user', name);
-      
-    }).then(() =>{
-      let token = sessionStorage.getItem('jwt');
-      $.ajax({
-        url: 'http://localhost:3000/account/status',
-        method: 'get',
-      
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-        withCredentials: true,
-        success: function(data){
-          res = data;
-        }
-      }).then(() => {
-
-        // console.log(res.user);
-        sessionStorage.setItem('age', res.user.age);
-        sessionStorage.setItem('sign', res.user.sign);
-      });}).then(() =>{
-        let token = sessionStorage.getItem('jwt');
-        $.ajax({
-          url: 'http://localhost:3000/user/profile',
-          method: 'get',
-        
-          headers: {
-            Authorization: `Bearer ${token}`
-          },
-          withCredentials: true,
-          success: function(data){
-            res = data;
-          }
-        }).then(() => {
-  
-          // console.log(res.user);
-          sessionStorage.setItem('name', profile.name);
-          sessionStorage.setItem('interests', profile.interests);
-          sessionStorage.setItem('bio', profile.bio);
-          location.href = ("../interface/profile/index.html");
-          
-        }).catch(() =>{
-          location.href = ("../interface/profile/index.html");
-        });}).catch(() => {
-      $message.html('<span class="has-text-danger">Something went wrong and you were not logged in. Check your email and password and your internet connection.</span>');
+      second();
+    }).catch(() => {
+      $message.html('<span class="has-text-danger">Something went wrong and you were not verified. Check your email and password and your internet connection.</span>');
     });
   });
 });

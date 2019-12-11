c90b30b594a57511defb486ffe6e3cd082731baf
Jairo-Rubio <jairorub@live.unc.edu>
2019-12-11 12:42:35 -0500

    home andmatches button no longer bring up edit profile

--- interface/profile/display.js
+++ interface/profile/display.js
@@ -30,8 +30,8 @@ export const renderprofileCard = function(profile) {
   <div class="yourProfile">
   <p class="is-4 titleText" style="color:purple; font-size: 60px; text-align: center;">Welcome ${user}</p>
   <div style="text-align: center;">
-  <button <a class="button is-rounded EditButton headerText" style="color:purple; padding: 5px; margin: 5px;" >Home</a></button>
-  <button <a class="button is-rounded EditButton headerText" style="color:purple; padding: 5px; margin: 5px;" >Matches</a></button>
+  <button <a class="button is-rounded  headerText" style="color:purple; padding: 5px; margin: 5px;" >Home</a></button>
+  <button <a class="button is-rounded  headerText" style="color:purple; padding: 5px; margin: 5px;" >Matches</a></button>
   <button <a href="index.html" class="button is-rounded headerText" style="color:purple; padding: 5px; margin: 5px;" >My Profile</a></button>
   </div>
   
